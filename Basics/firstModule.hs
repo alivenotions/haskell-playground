3abd90ca4a68573e15e9587ade8da1224dfb2ed6
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-07-27 20:10:44 +0530

    basic syntax and feel good examples

---
+++ Basics/firstModule.hs
@@ -0,0 +1,9 @@
+module FirstModule where
+-- This is to show that in a source code the order
+-- of declarations do not matter
+
+x = 10 * 5 + y
+
+myResult = x * 5
+
+y = 10
---
+++ Basics/firstSteps.hs
@@ -0,0 +1,24 @@
+-- getting familiar with the syntax
+
+sayHello :: String -> IO ()
+sayHello x = putStrLn("Hello, " ++ x  ++ "!")
+
+triple :: Integer -> Integer
+triple x = x * 3
+
+square :: Integer -> Integer
+square x = x ^ 2
+
+add :: Integer -> Integer -> Integer
+add x y = x + y
+
+adderMultiplier :: 
+  (Integer -> Integer) ->
+      (Integer -> Integer -> Integer) ->
+        Integer ->
+          Integer ->
+            Integer
+adderMultiplier f g x y = f (g x y)
+
+areaOfCircle :: Float -> Float
+areaOfCircle r = 3.14 * r * r
