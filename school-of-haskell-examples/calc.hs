f678070f22334ee5fd4d67f536f39a61da6ce061
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-08-20 21:14:34 +0530

    implement tokenize

--- school-of-haskell-examples/calc.hs
+++ school-of-haskell-examples/calc.hs
@@ -1,8 +1,30 @@
-data Token
+module Calc where
+import Data.Char
+
+data Token = TokOp Operator
+            | TokIdent String
+            | TokNum Int
+      deriving (Show, Eq)
+
+data Operator = Plus | Minus | Times | Div
+  deriving (Show, Eq)
+
 data Expression
 
+operator :: Char -> Operator
+operator c  | c == '+' = Plus
+            | c == '-' = Minus
+            | c == '*' = Times
+            | c == '/' = Div
+
 tokenize :: String -> [Token]
-tokenize = undefined
+tokenize [] = []
+tokenize (c : cs)
+  | elem c "+-*/" = TokOp (operator c)  : tokenize cs
+  | isDigit c = TokNum (digitToInt c)   : tokenize cs
+  | isAlpha c = TokIdent [c]            : tokenize cs
+  | isSpace c = tokenize cs
+  | otherwise = error $ "Cannot tokenize " ++ [c]
 
 parse :: [Token] -> Expression
 parse = undefined
@@ -10,20 +32,17 @@ parse = undefined
 evaluate :: Expression -> Double
 evaluate = undefined
 
-loop :: Int -> IO ()
-loop n = do
-  if n < 11
-  then do
-    putStrLn $ show (n * n)
-    loop $ n + 1
-  else 
-    return ()
-
-fact :: Int -> Int
-fact n = if n > 0 then n * fact (n - 1) else 1
-
-fullFact :: Integer -> Integer
-fullFact n = if n > 0 then n * fullFact (n - 1) else 1
-
 main :: IO ()
-main = print $ fullFact 23
+main = print $ tokenize " 1 + 4 / x "
+
+
+-- opToStr :: Operator -> String
+-- opToStr Plus = "+"
+-- opToStr Minus = "-"
+-- opToStr Times = "*"
+-- opToStr Div = "/"
+
+-- showContent :: Token -> String
+-- showContent (TokOp op) = opToStr op
+-- showContent (TokIdent str) = str
+-- showContent (TokNum i) = show i
